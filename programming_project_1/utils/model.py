43a73a0818eef2bde67c7b448b224c860e0cc9a8
liu-shuj <43320966+liu-shuj@users.noreply.github.com>
2019-12-29 21:04:40 -0500

    update

--- programming_project_1/utils/model.py
+++
@@ -1,7 +0,0 @@
-def MergeModel(model1:dict,model2:dict)->dict:
-    for key in model1:
-        if key in model2:
-            model2[key]=(model1[key]+model2[key])/2
-        else:
-            model2[key]=model1[key]
-    return model2
